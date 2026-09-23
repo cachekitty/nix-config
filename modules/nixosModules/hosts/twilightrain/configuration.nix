c9d906babab596eabfd8bfed49314ec3abb91a0a
cachekitty <contact@cachekitty.fyi>
2026-09-23 19:53:17 +1000

    Adding virtualisation module to host

--- modules/nixosModules/hosts/twilightrain/configuration.nix
+++ modules/nixosModules/hosts/twilightrain/configuration.nix
@@ -14,6 +14,7 @@
       self.nixosModules.base
       self.nixosModules.general
       self.nixosModules.desktop
+      self.nixosModules.virtualisation
     ];
 
     boot.loader.systemd-boot.enable = true;
