72ca85908258ae35b7377c6423f5d78374e3843a
cachekitty <contact@cachekitty.fyi>
2026-09-24 19:51:42 +1000

    Remove regreet for now

--- modules/nixosModules/features/desktop.nix
+++ modules/nixosModules/features/desktop.nix
@@ -21,14 +21,12 @@
     programs.niri.enable = true;
     programs.niri.package = selfpkgs.niri;
 
-    programs.regreet.enable = true;
-
     services.greetd = {
         enable = true;
         settings = {
             default_session = {
                 command = "${config.programs.niri.package}/bin/niri-session";
-                user = "greeter";
+                user = "cachekitty";
             };
         };
     };
